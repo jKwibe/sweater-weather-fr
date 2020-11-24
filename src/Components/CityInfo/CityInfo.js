0a75168b09c2d571fd7e6e8799dce5fb2b04a2b2
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-11-24 01:06:34 -0700

    Add data import functionality

--- src/Components/CityInfo/CityInfo.js
+++ src/Components/CityInfo/CityInfo.js
@@ -2,9 +2,49 @@ import React, {Component} from "react";
 import "./CityInfo.css"
 
 class CityInfo extends Component{
+
     render() {
+        let unixTime = Date.now()
+        let formattedDate = new Date(unixTime)
+        let date = formattedDate.toLocaleString('en-US', {
+            weekday: 'short',
+            month: '2-digit',
+            year: '2-digit'
+        })
+
+        let time = formattedDate.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric'
+        })
+
         return(
             <section className="city-info">
+                <ul>
+                    <li>
+                        { this.props.info !== null && this.props.info.data.attributes.city }
+                    </li>
+
+                    <li>
+                        { this.props.info !== null && this.props.info.data.attributes.state }
+                    </li>
+
+                    <li>
+                        { this.props.info !== null && this.props.info.data.attributes.country }
+                    </li>
+
+                    <li>
+                        { this.props.info !== null && this.props.info.data.attributes.current.temp }
+                    </li>
+
+                    <li>
+                        { date }
+                    </li>
+
+                    <li>
+                        { time }
+                    </li>
+
+                </ul>
             </section>
         )
     }
--- src/Components/MainArea/MainCanvas.js
+++ src/Components/MainArea/MainCanvas.js
@@ -9,22 +9,21 @@ import "./MainCanvas.css"
 
 
 class MainCanvas extends Component{
-    constructor(props) {
-        super(props);
-        this.getData().then(data => console.log(data)).catch(e => console.error(e))
-        this.state = {
-            forecast: {}
+        state = {
+            forecast: null
         }
-
+    async componentDidMount() {
+        this.fetchData().then()
     }
 
-    async getData(){
-        let res = await axios.get("https://sweater-weather-api-rails.herokuapp.com/api/v1/forecast",
-            {
-                params:{
-                    location: 'denver,co'
+    async fetchData() {
+        let res = await
+            axios.get("https://sweater-weather-api-rails.herokuapp.com/api/v1/forecast",
+                {
+                    params:{
+                        location: 'denver,co'
+                    }
                 }
-            }
             );
 
         this.setState({
@@ -33,11 +32,12 @@ class MainCanvas extends Component{
     }
 
     render() {
+        const { forecast } = this.state;
         return(
             <main className="main-section">
                 <section className="top-city-info row">
                     <article className="left-city-info col-6">
-                        <CityInfo/>
+                        <CityInfo info={forecast}/>
                     </article>
 
                     <article className="right-city-info col-6">
