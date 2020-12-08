c74e923bc7ca1d21fd0cb1a06d921b58995fdb95
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-12-08 00:43:40 -0700

    Add weekly forecast data

--- src/Components/WeaklyForecast/WeaklyForecast.js
+++ src/Components/WeaklyForecast/WeaklyForecast.js
@@ -1,11 +1,21 @@
 import React, {Component} from "react";
 import "./WeaklyForecast.css"
 
+import Hourly from "./Hourly/Hourly";
+import Daily from "./Daily/Daily";
+
 class WeaklyForecast extends Component{
     render() {
+        let hourlyData ;
+        let dailyData;
+        if(this.props.info !== null){
+            hourlyData = this.props.info.data.attributes.hourly
+            dailyData = this.props.info.data.attributes.daily
+        }
         return(
             <section className="weakly-forecast">
-
+                <Hourly hourlyData = {hourlyData}/>
+                <Daily dailyData = {dailyData}/>
             </section>
         )
     }
