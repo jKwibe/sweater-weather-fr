5c97099a40453a216fb08bd29f6e013f370c48d0
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-12-15 00:11:57 -0700

    Add the route components to the navbar.

--- src/Components/SweaterWeather.js
+++ src/Components/SweaterWeather.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react"
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import MainNavbar from "./Navbar/Navbar";
 import MainCanvas from "./MainArea/MainCanvas";
 import Footer from "./Footer/Footer";
+import Switch from "react-bootstrap/Switch";
+
+import SignIn from "./SignIn/SignIn"
+import SignUp from "./SignUp/SignUp"
 
 class SweaterWeather extends Component {
     render() {
         return(
             <section className="container">
                 <MainNavbar/>
-                <MainCanvas/>
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={MainCanvas}/>
+                        <Route path="/signin" exact component={SignIn}/>
+                        <Route path="/signup" exact component={SignUp}/>
+                    </Switch>
+                </Router>
                 <Footer/>
             </section>
         )
