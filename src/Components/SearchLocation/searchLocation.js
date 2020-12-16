69f276b9d7f3ea1ebaac10055f715b6a5648f021
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-12-16 16:23:02 -0700

    Add a search location form to seach for a new location.

---
+++ src/Components/SearchLocation/searchLocation.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+
+class SearchLocation extends Component{
+    state = {
+        inputText:''
+    }
+
+    onLocChange = (event)=>{
+        event.preventDefault()
+        this.props.onSubmit(this.state.inputText)
+        this.setState({
+            inputText:''
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            inputText: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <section>
+                <form onSubmit={this.onLocChange}>
+                    <input type="text" value={this.state.inputText} onChange={ this.handleChange }/>
+                    <input type="submit" value="Find Location"/>
+                </form>
+            </section>
+        )
+    }
+}
+
+export default SearchLocation
